6493bcaa4c69c29c18d1c5e8d0c8cbf4a75ec381
Joe Hart <joe@jeverest.com>
2025-12-09 12:21:38 -0800

    Update content/_meta.ts
    Updated via Jolli repository editor

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,23 +1,36 @@
 export default {
   'index': 'Home',
+
+  'general': { type: 'separator', title: 'General' },
   'linkace-translations': 'LinkAce Translations',
   'linkace-v2-changelog': 'LinkAce v2 Changelog',
   'third-party-tools-libraries': 'Third-Party Tools & Libraries',
   'troubleshooting': 'Troubleshooting',
+
+  'linkace-setup': { type: 'separator', title: 'LinkAce Setup' },
   'setup-with-docker': 'Setup with Docker',
   'setup-with-helm-kubernetes': 'Setup with Helm (Kubernetes)',
   'one-click-deploy-to-cloud': 'One-Click-Deploy to Cloud',
   'setup-without-docker': 'Setup without Docker',
   'post-setup-steps': 'Post-Setup Steps',
   'adjusting-the-database-collation': 'Adjusting the Database Collation',
+
+  'linkace-upgrade-guide-separator': { type: 'separator', title: 'LinkAce Upgrade Guide' },
   'linkace-upgrade-guide': 'LinkAce Upgrade Guide',
   'upgrade-from-linkace-v1': 'Upgrade from LinkAce v1',
+
+  'about-the-application': { type: 'separator', title: 'About the Application' },
   'the-dashboard': 'The Dashboard',
   'links': 'Links',
   'lists': 'Lists',
   'link-checks': 'Link Checks',
+
+  'configuration-of-linkace': { type: 'separator', title: 'Configuration of LinkAce' },
+
+  'cli-commands-for-linkace': { type: 'separator', title: 'CLI Commands for LinkAce' },
+
   'api-linkace-openapi': {
-    title: 'Linkace Openapi',
+    title: 'LinkAce OpenAPI',
     type: 'page',
     href: '/api-docs-linkace-openapi.html'
   }
