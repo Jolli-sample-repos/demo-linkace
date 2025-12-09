e012611bdc71d05f73465b11c2e0a6b9b74bf709
Joe Hart <joe@jeverest.com>
2025-12-09 12:10:46 -0800

    Update documentation (preserve custom files)

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,36 +1,21 @@
 export default {
   'index': 'Home',
-
-  'General': { type: 'separator', title: 'General' },
   'linkace-translations': 'LinkAce Translations',
   'linkace-v2-changelog': 'LinkAce v2 Changelog',
   'third-party-tools-libraries': 'Third-Party Tools & Libraries',
   'troubleshooting': 'Troubleshooting',
-
-  'LinkAce Setup': { type: 'separator', title: 'LinkAce Setup' },
   'setup-with-docker': 'Setup with Docker',
   'setup-with-helm-kubernetes': 'Setup with Helm (Kubernetes)',
   'one-click-deploy-to-cloud': 'One-Click-Deploy to Cloud',
   'setup-without-docker': 'Setup without Docker',
   'post-setup-steps': 'Post-Setup Steps',
   'adjusting-the-database-collation': 'Adjusting the Database Collation',
-
-  'LinkAce Upgrade Guide': { type: 'separator', title: 'LinkAce Upgrade Guide' },
   'linkace-upgrade-guide': 'LinkAce Upgrade Guide',
   'upgrade-from-linkace-v1': 'Upgrade from LinkAce v1',
-
-  'About the Application': { type: 'separator', title: 'About the Application' },
   'the-dashboard': 'The Dashboard',
   'links': 'Links',
   'lists': 'Lists',
-  'link-checks': 'Link Checks'
-
-  'Configuration of LinkAce': { type: 'separator', title: 'Configuration of LinkAce' },
-  
-
-  'CLI Commands for LinkAce': { type: 'separator', title: 'CLI Commands for LinkAce' },
-
-
+  'link-checks': 'Link Checks',
   'api-linkace-openapi': {
     title: 'Linkace Openapi',
     type: 'page',
