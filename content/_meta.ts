769e5403710c4705d5457117b8e3399257fcbe80
Joe Hart <joe@jeverest.com>
2025-12-12 15:20:25 -0800

    Update content/_meta.ts
    Updated via Jolli repository editor

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,18 +1,33 @@
 export default {
-  'index': { display: 'hidden' },
-  'linkace-overview': 'LinkAce Overview',
+  'index': 'Home',
+  'general': {
+    type: 'separator',
+    title: 'General'
+  },
   'linkace-translations': 'LinkAce Translations',
   'linkace-v2-changelog': 'LinkAce v2 Changelog',
   'third-party-tools-libraries': 'Third-Party Tools & Libraries',
   'troubleshooting': 'Troubleshooting',
+  'linkace-setup': {
+    type: 'separator',
+    title: 'LinkAce Setup'
+  },
   'setup-with-docker': 'Setup with Docker',
   'setup-with-helm-kubernetes': 'Setup with Helm (Kubernetes)',
   'one-click-deploy-to-cloud': 'One-Click-Deploy to Cloud',
   'setup-without-docker': 'Setup without Docker',
   'post-setup-steps': 'Post-Setup Steps',
   'adjusting-the-database-collation': 'Adjusting the Database Collation',
+  'linkace-upgrade-guide-separator': {
+    type: 'separator',
+    title: 'LinkAce Upgrade Guide'
+  },
   'linkace-upgrade-guide': 'LinkAce Upgrade Guide',
   'upgrade-from-linkace-v1': 'Upgrade from LinkAce v1',
+  'about-the-application': {
+    type: 'separator',
+    title: 'About the Application'
+  },
   'the-dashboard': 'The Dashboard',
   'links': 'Links',
   'link-checks': 'Link Checks',
@@ -22,6 +37,10 @@ export default {
   'bookmarklet': 'Bookmarklet',
   'rss-feeds': 'RSS Feeds',
   'trash': 'Trash',
+  'configuration-of-linkace': {
+    type: 'separator',
+    title: 'Configuration of LinkAce'
+  },
   'import-links': 'Import Links',
   'export-links': 'Export Links',
   'user-settings': 'User Settings',
@@ -32,10 +51,15 @@ export default {
   'sso-single-sign-on-with-oauth-or-oidc': 'SSO (Single-Sign-On) with OAuth or OIDC',
   'application-backups': 'Application Backups',
   'advanced-settings': 'Advanced Settings',
+  'cli-commands-linkace': {
+    type: 'separator',
+    title: 'CLI Commands for LinkAce'
+  },
   'cli-commands-for-linkace': 'CLI Commands for LinkAce',
-  'api-reference': {
+  'api-linkace-openapi': {
     title: 'API Reference',
     type: 'page',
-    href: '/api-docs/linkace-openapi'
+    href: '/api-docs-linkace-openapi.html'
   }
 }
+
